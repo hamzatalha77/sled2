a1d4bcf24dc35a80971ba88b1e9f5af17e977d3a
hamzatalha77 <hamza_ta7@hotmail.com>
2024-07-30 14:09:15 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import Header from './components/layouts/header'
 import Collab from './components/layouts/collab'
 import About from './components/layouts/about'
+import News from './components/layouts/news'
+
 const App = () => {
   return (
     <div>
       <Header />
       <Collab />
       <About />
+      <News />
     </div>
   )
 }
--- src/components/layouts/news.tsx
+++ src/components/layouts/news.tsx
@@ -1,7 +1,72 @@
 import React from 'react'
-
+import News1 from '../../assets/images/news1.jpg'
+import News2 from '../../assets/images/news2.jpg'
+import News3 from '../../assets/images/news3.jpg'
 const News = () => {
-  return <div></div>
+  return (
+    <section id="blog">
+      <div className="blog-heading">
+        <span>Our Recent Posts</span>
+        <h3>My Blog</h3>
+      </div>
+      <div className="blog-container">
+        <div className="blog-box">
+          <div className="blog-img">
+            <img src={News1} alt="blog" />
+          </div>
+          <div className="blog-text">
+            <span>18 july 2024 / web design</span>
+            <a href="#" className="blog-title">
+              what has happened of all of the web design ideas?
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              quo voluptate ex eveniet accusantium a deserunt! Quaerat, impedit
+              deleniti. Aliquam, minus voluptatibus? Doloribus eaque sint
+              repellat et tenetur, ex minus.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+        <div className="blog-box">
+          <div className="blog-img">
+            <img src={News1} alt="blog" />
+          </div>
+          <div className="blog-text">
+            <span>18 july 2024 / web design</span>
+            <a href="#" className="blog-title">
+              what has happened of all of the web design ideas?
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              quo voluptate ex eveniet accusantium a deserunt! Quaerat, impedit
+              deleniti. Aliquam, minus voluptatibus? Doloribus eaque sint
+              repellat et tenetur, ex minus.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+        <div className="blog-box">
+          <div className="blog-img">
+            <img src={News1} alt="blog" />
+          </div>
+          <div className="blog-text">
+            <span>18 july 2024 / web design</span>
+            <a href="#" className="blog-title">
+              what has happened of all of the web design ideas?
+            </a>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore
+              quo voluptate ex eveniet accusantium a deserunt! Quaerat, impedit
+              deleniti. Aliquam, minus voluptatibus? Doloribus eaque sint
+              repellat et tenetur, ex minus.
+            </p>
+            <a href="#">Read More</a>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default News
