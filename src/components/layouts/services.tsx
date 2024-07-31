3c5738d513df528393320b11d18ccec209fda27a
hamzatalha77 <hamza_ta7@hotmail.com>
2024-07-31 12:29:28 +0100

    commit

--- src/components/layouts/services.tsx
+++ src/components/layouts/services.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import 'remixicon/fonts/remixicon.css'
 const Services = () => {
   return (
     <section className="services-container">
       <h1>Our Services</h1>
       <div className="services-row">
         <div className="services">
+          <i className="ri-terminal-box-line"></i>
           <h2>Web Design</h2>
           <p>
             Lorem ipsum dolor sit amet consectetur adipisicing elit. Laudantium,
