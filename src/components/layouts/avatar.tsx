4a3f73739a550a21d634643da18679dbb289cec5
hamzatalha77 <hamza_ta7@hotmail.com>
2024-08-01 12:42:19 +0100

    commit

---
+++ src/components/layouts/avatar.tsx
@@ -0,0 +1,123 @@
+import Avatar1 from '../../assets/img/avatar-1.png'
+import Avatar2 from '../../assets/img/avatar-2.png'
+import Avatar3 from '../../assets/img/avatar-3.png'
+import Avatar4 from '../../assets/img/avatar-4.png'
+import Avatar5 from '../../assets/img/avatar-5.png'
+const Avatar = () => {
+  return (
+    <section className="container77">
+      <div className="card__container swiper">
+        <div className="card__content">
+          <div className="swiper-wrapper">
+            <article className="card__article swiper-slide">
+              <div className="card__image">
+                <img src={Avatar1} alt="image" className="card__img" />
+                <div className="card__shadow"></div>
+              </div>
+
+              <div className="card__data">
+                <h3 className="card__name">Kell Dawx</h3>
+                <p className="card__description">
+                  Passionate about development and design, I carry out projects
+                  at the request of users.
+                </p>
+
+                <a href="#" className="card__button">
+                  View More
+                </a>
+              </div>
+            </article>
+
+            <article className="card__article swiper-slide">
+              <div className="card__image">
+                <img src={Avatar2} alt="image" className="card__img" />
+                <div className="card__shadow"></div>
+              </div>
+
+              <div className="card__data">
+                <h3 className="card__name">Lotw Fox</h3>
+                <p className="card__description">
+                  Passionate about development and design, I carry out projects
+                  at the request of users.
+                </p>
+
+                <a href="#" className="card__button">
+                  View More
+                </a>
+              </div>
+            </article>
+
+            <article className="card__article swiper-slide">
+              <div className="card__image">
+                <img src={Avatar3} alt="image" className="card__img" />
+                <div className="card__shadow"></div>
+              </div>
+
+              <div className="card__data">
+                <h3 className="card__name">Sara Mit</h3>
+                <p className="card__description">
+                  Passionate about development and design, I carry out projects
+                  at the request of users.
+                </p>
+
+                <a href="#" className="card__button">
+                  View More
+                </a>
+              </div>
+            </article>
+
+            <article className="card__article swiper-slide">
+              <div className="card__image">
+                <img src={Avatar4} alt="image" className="card__img" />
+                <div className="card__shadow"></div>
+              </div>
+
+              <div className="card__data">
+                <h3 className="card__name">Jenny Wert</h3>
+                <p className="card__description">
+                  Passionate about development and design, I carry out projects
+                  at the request of users.
+                </p>
+
+                <a href="#" className="card__button">
+                  View More
+                </a>
+              </div>
+            </article>
+
+            <article className="card__article swiper-slide">
+              <div className="card__image">
+                <img src={Avatar5} alt="image" className="card__img" />
+                <div className="card__shadow"></div>
+              </div>
+
+              <div className="card__data">
+                <h3 className="card__name">Lexa Kin</h3>
+                <p className="card__description">
+                  Passionate about development and design, I carry out projects
+                  at the request of users.
+                </p>
+
+                <a href="#" className="card__button">
+                  View More
+                </a>
+              </div>
+            </article>
+          </div>
+        </div>
+
+        <div className="swiper-button-next">
+          <i className="ri-arrow-right-s-line"></i>
+        </div>
+
+        <div className="swiper-button-prev">
+          <i className="ri-arrow-left-s-line"></i>
+        </div>
+
+        <div className="swiper-pagination"></div>
+      </div>
+    </section>
+  )
+}
+
+export default Avatar
