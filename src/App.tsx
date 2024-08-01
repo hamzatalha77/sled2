da2402225d651973ffe5eb77e7af8ef1adcced8d
hamzatalha77 <hamza_ta7@hotmail.com>
2024-08-01 15:15:19 +0100

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -4,11 +4,13 @@ import About from './components/layouts/about'
 import News from './components/layouts/news'
 import Services from './components/layouts/services'
 import Products from './components/layouts/products'
+import Hero from './components/layouts/hero'
 
 const App = () => {
   return (
     <div>
       <Header />
+      <Hero />
       <Collab />
       <About />
       <News />
--- src/components/layouts/hero.tsx
+++ src/components/layouts/hero.tsx
@@ -1,7 +1,125 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Swiper from 'swiper'
+import { Navigation, Pagination } from 'swiper/modules'
+import 'swiper/css'
+import 'swiper/css/navigation'
+import 'swiper/css/pagination'
+import bgImage1 from '../../assets/img/bg1.jpg'
+import bgImage2 from '../../assets/img/bg2.jpg'
+import bgImage3 from '../../assets/img/bg3.jpg'
+import icon1 from '../../assets/icons/icons8-facebook.svg'
+import icon2 from '../../assets/icons/icons8-insta.svg'
+import icon3 from '../../assets/icons/icons8-telegram.svg'
 
 const Hero = () => {
-  return <div></div>
+  useEffect(() => {
+    Swiper.use([Navigation, Pagination])
+
+    new Swiper('.swiper', {
+      speed: 1800,
+      loop: true,
+      pagination: {
+        el: '.swiper-progressbar',
+        type: 'progressbar',
+        clickable: true
+      },
+      navigation: {
+        nextEl: '.swiper-button-next',
+        prevEl: '.swiper-button-prev'
+      }
+    })
+  }, [])
+
+  return (
+    <section className="h-screen overflow-hidden relative text-white">
+      <div className="swiper">
+        <div className="swiper-wrapper">
+          <div className="swiper-slide">
+            <div
+              className="slider-content"
+              style={{ backgroundImage: `url(${bgImage1})` }}
+            >
+              <div className="slider-heading">
+                <h2 className="hero-title">
+                  <span>Discover the best</span>
+                </h2>
+                <h2 className="hero-title">
+                  <span>we are the lights</span>
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="slider-content"
+              style={{ backgroundImage: `url(${bgImage3})` }}
+            >
+              <div className="slider-heading">
+                <h2>
+                  <span>Discover the best</span>
+                </h2>
+                <h2>
+                  <span>we are the lights</span>
+                </h2>
+              </div>
+            </div>
+          </div>
+          <div className="swiper-slide">
+            <div
+              className="slider-content"
+              style={{ backgroundImage: `url(${bgImage2})` }}
+            >
+              <div className="slider-heading">
+                <h2>
+                  <span>Discover the best</span>
+                </h2>
+                <h2>
+                  <span>we are the lights</span>
+                </h2>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div className="video-btn">
+        <a href="#">
+          <span></span>
+        </a>
+      </div>
+      <div className="social-icons-wrap">
+        <ul>
+          <li>
+            <a href="#">
+              <img src={icon1} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={icon2} alt="" />
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <img src={icon3} alt="" />
+            </a>
+          </li>
+        </ul>
+        <div className="h-line-before"></div>
+        <div className="h-line-after"> </div>
+      </div>
+      <div className="copyright-text">
+        Copyright &#169; 2024 all rights reserved.
+      </div>
+      <div className="swiper-progressbar-wrap">
+        <div className="active-slider"></div>
+        <div className="swiper-progressbar"></div>
+        <div className="total-slider"></div>
+      </div>
+      <div className="swiper-pagination"></div>
+      <div className="swiper-button-prev"></div>
+      <div className="swiper-button-next"></div>
+    </section>
+  )
 }
 
 export default Hero
