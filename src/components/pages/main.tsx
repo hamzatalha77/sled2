425c18494d4b44c73b5a48ea3c0a060fb9459799
hamzatalha77 <hamza_ta7@hotmail.com>
2024-08-08 19:49:04 +0100

    commit

--- src/components/pages/main.tsx
+++ src/components/pages/main.tsx
@@ -6,12 +6,14 @@ import News from '../sections/news'
 // import Services from '../sections/services'
 import Moving from '../sections/moving'
 import Clients from '../sections/clients'
+import Steps from '../sections/steps'
 
 const Main = () => {
   return (
     <div>
       <Hero />
       <Aboutus />
+      <Steps />
       <Moving />
       <Products />
       <Clients />
---
+++ src/components/sections/steps.tsx
@@ -0,0 +1,55 @@
+const Steps = () => {
+  return (
+    <section>
+      <div className="big-headers">
+        <span className="big-header">Our Steps</span>
+      </div>
+      <div className="container px-5 py-24 mx-auto flex flex-wrap">
+        <div className="h-full p-4 lg:w-1/3">
+          <div className="  px-8 pt-16 pb-16 relative">
+            <span className="text-center my-8 items-center flex justify-center">
+              <i className="ri-compasses-2-fill text-5xl text-[#179de5] font-medium"></i>
+            </span>
+            <h1 className="title-font sm:text-2xl text-xl  mb-3 text-center">
+              Project Planning
+            </h1>
+            <p className="mb-3 text-center">
+              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+              microdosing tousled waistcoat.
+            </p>
+          </div>
+        </div>
+        <div className="h-full p-4 lg:w-1/3">
+          <div className="  px-8 pt-16 pb-16 relative">
+            <span className="text-center my-8 items-center flex justify-center">
+              <i className="ri-magic-line text-5xl text-[#179de5] font-medium"></i>
+            </span>
+            <h1 className="title-font sm:text-2xl text-xl  mb-3 text-center">
+              Gaining Materials
+            </h1>
+            <p className="mb-3 text-center">
+              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+              microdosing tousled waistcoat.
+            </p>
+          </div>
+        </div>
+        <div className="h-full p-4 lg:w-1/3">
+          <div className="  px-8 pt-16 pb-16 relative">
+            <span className="text-center my-8 items-center flex justify-center">
+              <i className="ri-tools-fill text-5xl text-[#179de5] font-medium"></i>
+            </span>
+            <h1 className="title-font sm:text-2xl text-xl  mb-3 text-center">
+              Project Execution
+            </h1>
+            <p className="mb-3 text-center">
+              Photo booth fam kinfolk cold-pressed sriracha leggings jianbing
+              microdosing tousled waistcoat.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
+}
+
+export default Steps
